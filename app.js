2e5dfcbaaf4646f0507862c3340457482e1716b3
Rogerswhale7 <144028418+Rogerswhale7@users.noreply.github.com>
2023-09-04 05:54:42 -0700

    Update app.js

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ document.getElementById('form').addEventListener('submit', function (event) {
     };
 
     // Send the form data via Email.js
-    emailjs.send("service_cczcb0i", "template_k7ixms9", formData)
+    emailjs.send("service_kiib6ok", "template_x2ru9ga", formData)
         .then(function (response) {
             console.log('SUCCESS!', response.status, response.text);
             redirectToURL();
